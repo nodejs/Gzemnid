00124eb3b848b0853c8dcc88774131af0e31cb8f
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-02-05 23:24:47 +0300

    Updates: use babel, gulp. Cleanup parse.js

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp = require('gulp');
+var changed = require('gulp-changed');
+var plumber = require('gulp-plumber');
+var babel = require('gulp-babel');
+
+var files = {
+	libs: 'libs/**/*.js'
+};
+
+var babelOptions = {
+	presets: ['es2015', 'stage-0']
+};
+
+gulp.task('libs', function () {
+	gulp.src(files.libs)
+		.pipe(plumber())
+		.pipe(changed('build'))
+		.pipe(babel(babelOptions))
+		.pipe(babel())
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('build', ['libs']);
+
+gulp.task('default', ['build'], function() {
+	gulp.watch(files.libs, ['libs']);
+});
---
+++ libs/parse.js
@@ -0,0 +1,58 @@
+'use strict';
+
+require('babel-polyfill');
+const fs = require('fs');
+const bhttp = require('bhttp');
+const JSONStream = require('JSONStream');
+
+const registryUrl = 'https://skimdb.npmjs.com/registry/_design/scratch/_view/byField';
+
+async function process() {
+	const source = await bhttp.get(registryUrl, {stream: true});
+	const stream = source.pipe(JSONStream.parse('rows.*'));
+
+	const out = fs.createWriteStream('byField.info.json');
+	out.write('[\n')
+
+	let count = 0;
+	stream.on('data', (data) => {
+		if (data.id !== data.key || data.id !== data.value.name) {
+			console.log('UNEXPECTED: ' + JSON.stringify({
+				id: data.id,
+				key: data.key,
+				'value.name': data.value.name
+			}));
+			//console.log('received:', data);
+			return;
+		}
+		data = data.value;
+		const info = {
+			id: data.name + '-' + data.version,
+			name: data.name,
+			version: data.version,
+			url: data.bugs && data.bugs.url || data.homepage || data.repository && data.repository.url || null,
+			user: data._npmUser,
+			npm: data._npmVersion,
+			node: data._nodeVersion,
+			tar: data.dist.tarball
+		};
+
+		if (count > 0) {
+			out.write(',\n');
+		}
+		out.write(JSON.stringify(info));
+
+		count++;
+		if (count % 1000 === 0) {
+			console.log(count + '...');
+		}
+	});
+
+	stream.on('end', () => {
+		console.log(count + '.');
+		out.write('\n]\n');
+		console.log('END');
+	});
+}
+
+process();
