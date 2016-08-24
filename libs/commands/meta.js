f93bdbd402ea7d2135ec7bd8e538cf1a0b018ce4
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-08-24 22:57:05 +0300

    meta: download files directly instead of building a list

--- libs/commands/meta.js
+++ libs/commands/meta.js
@@ -3,21 +3,24 @@
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 const path = require('path');
+const bhttp = require('bhttp');
 const config = require('../config').config;
 const { jsonStream, toMap, mkdirpAsync } = require('../helpers');
+
+const session = bhttp.session({
+  headers: {
+    'user-agent': config.useragent || 'Gzemnid'
+  }
+});
 
 async function run() {
   await mkdirpAsync(path.join(config.dir, 'meta/'));
   const current = await fs.readdirAsync(path.join(config.dir, 'meta/'));
   const map = toMap(current);
 
-  const out = {
-    wget: fs.createWriteStream(path.join(config.dir, 'meta.wget.txt'))
-  };
-
   let count = 0;
-  let updated = 0;
   const stream = jsonStream('byField.info.json');
+  const queue = [];
   stream.on('data', info => {
     if (!info.tar) {
       console.log(`${info.id}: no tar!`);
@@ -27,20 +30,31 @@ async function run() {
     const url = `https://registry.npmjs.org/${info.name}`;
     const file = `${info.id}.json`;
     if (!map.has(file)) {
-      out.wget.write(`wget -nv -nc ${url} -O ${file}\n`);
-      updated++;
+      queue.push([url, file]);
     }
 
     map.set(file, true);
     count++;
-    if (count % 10000 === 0) {
-      console.log(`${count}...`);
+    if (count % 50000 === 0) {
+      console.log(`Checking: ${count}...`);
     }
   });
 
   await stream.promise;
-
   console.log(`Total: ${count}.`);
+
+  let updated = 0;
+  for (const [url, file] of queue) {
+    console.log(`Downloading: ${file}...`);
+    const out = path.join(config.dir, 'meta/', file);
+    const response = await session.get(url, { stream: true })
+    response.pipe(fs.createWriteStream(out));
+    updated++;
+    if (updated % 100 === 0) {
+      console.log(`Downloaded: ${updated}/${queue.length}...`);
+    }
+  }
+
   console.log(`New/updated: ${updated}.`);
   let moved = 0;
   for (const [file, keep] of map) {
