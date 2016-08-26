84d1ee589810ad71773f1e17590abdaa8761eab3
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-08-26 09:15:24 +0300

    examples: minor cleanup

--- examples/ast_status.js
+++ examples/ast_status.js
@@ -1,7 +1,6 @@
 module.exports = function(stream) {
   stream.on('data', ({ key, value }) => {
-    const path = key;
-    const ast = value;
+    const [path, ast] = [key, value];
     const status = typeof ast === 'string' ? ast : 'ok';
     console.log(`[${status}] ${path}`);
   });
