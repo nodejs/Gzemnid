fd034ff485f2788bf7006ca456cb42b562a9360f
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-08-31 02:08:40 +0300

    code: add extensions whitelist, fix extension filter

--- libs/commands/code.js
+++ libs/commands/code.js
@@ -7,27 +7,26 @@ const { packedIn, promiseEvent } = require('../helpers');
 const readline = require('readline');
 
 const extensions = [
-  //'.php', '.json', '.txt',
-  '.ts', '.coffee', '.js'
+  'ts', 'coffee', 'js'
 ];
 
-async function search(query, ext) {
-  if (!ext) {
-    for (const entry of extensions) {
-      await search(query, entry);
-    }
-    return;
+async function search(query, languages = null) {
+  if (languages && typeof languages === 'string') {
+    languages = languages.split(',');
   }
-  const infile = path.join(config.dir, 'out/', `slim.code${ext}.txt`);
-  const stream = packedIn(infile, config.extract.compress);
-  const regex = new RegExp(query);
-  readline.createInterface({
-    input: stream
-  }).on('line', line => {
-    if (!regex.test(line)) return;
-    console.log(line);
-  });
-  await promiseEvent(stream);
+  for (const ext of extensions) {
+    if (languages && languages.indexOf(ext) === -1) return;
+    const infile = path.join(config.dir, 'out/', `slim.code.${ext}.txt`);
+    const stream = packedIn(infile, config.extract.compress);
+    const regex = new RegExp(query);
+    readline.createInterface({
+      input: stream
+    }).on('line', line => {
+      if (!regex.test(line)) return;
+      console.log(line);
+    });
+    await promiseEvent(stream);
+  }
 }
 
 module.exports = {
