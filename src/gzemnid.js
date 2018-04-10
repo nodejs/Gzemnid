c49ac2150aa40bfbca7d2ca31d3d9cf53dad6c14
Jamie Davis <davisjam@vt.edu>
2018-04-10 10:48:31 -0400

    docs: add usage message
    When invoked with no args, gzemnid will now print a usage message.

--- src/gzemnid.js
+++ src/gzemnid.js
@@ -14,15 +14,42 @@ const commands = {
   packages: require('./commands/packages')
 };
 
+function getUsageMessage() {
+  const cmdMessages = [
+    { cmd: 'fetch', desc: 'build byField.info.json' },
+    { cmd: 'stats', desc: '[rebuild|update]: rebuild or update stats.json, default rebuild' },
+    { cmd: 'meta', desc: 'build meta/' },
+    { cmd: 'debsdb', desc: '[plain|resolved|nested|stats]: invoke in succession ' +
+    ' to build up debsdp in stages, default will run all' },
+    { cmd: 'packages', desc: 'build current/' },
+    { cmd: 'extract', desc: '[partials|totals]: invoke in succession to XXX, default to XXX' },
+    { cmd: 'code search', desc: '{regex}: search code for lines matching this regex' },
+    { cmd: 'ast execute', desc: '{file.js}: walk all ASTs and apply code in this file' },
+    { cmd: 'server', desc: 'start web server' }
+  ];
+
+  let usageMessage = 'Usage: gzemnid COMMMAND';
+  cmdMessages.forEach(m => {
+    usageMessage += `\n  ${m.cmd}: ${m.desc}`;
+  });
+
+  return usageMessage;
+}
+
 async function main(argv) {
   await configManager.load();
-  argv.shift();
-  if (argv.length > 0 && argv[0].endsWith('gzemnid.js')) {
+
+  argv.shift(); // Discard 'node'
+  if (argv.length > 0 && argv[0].endsWith('gzemnid.js')) { // Discard 'gzemnid.js'
     argv.shift();
   }
+
+  // Need usage?
   if (argv.length === 0) {
-    throw new Error('No command specified!');
+    throw new Error(getUsageMessage());
   }
+
+  // Invalid command?
   const command = argv.shift();
   if (!commands.hasOwnProperty(command)) {
     throw new Error(`No such command: ${command}.`);
@@ -31,13 +58,16 @@ async function main(argv) {
   if (!commands[command][method]) {
     throw new Error(`No such method of command ${command}: ${method}.`);
   }
+  // Valid command.
+
+  //Set global event handlers.
+  process.on('exit', () => {
+    console.error(`RSS: ${process.memoryUsage().rss / (2 << 20)} MiB`);
+  });
+  process.on('SIGPIPE', () => process.exit());
+  process.on('SIGTERM', () => process.exit());
+
   commands[command][method](...argv);
 }
-
-process.on('exit', () => {
-  console.error(`RSS: ${process.memoryUsage().rss / (2 << 20)} MiB`);
-});
-process.on('SIGPIPE', () => process.exit());
-process.on('SIGTERM', () => process.exit());
 
 main(process.argv).catch(e => console.error(e.stack));
