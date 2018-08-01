126a9f149595740ed280ef55876ad4a8caebb755
Сковорода Никита Андреевич <chalkerx@gmail.com>
2018-08-01 20:05:47 +0300

    helpers: properly emit 'close' event on packedOut streams

--- src/helpers.js
+++ src/helpers.js
@@ -112,6 +112,7 @@ function packedOut(file, compress = true) {
   const encoder = lz4.createEncoderStream({
     highCompression: config.compression.high
   });
+  outstream.on('close', () => encoder.emit('close'));
   encoder.pipe(outstream);
   return encoder;
 }
